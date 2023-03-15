6805eff83d8d6e62cdfd744721c4e5d47a34193c
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-03-15 13:56:36 +0530

    Add files via upload

---
+++ MulDiv.java
@@ -0,0 +1,13 @@
+class MulDiv{
+public static void main(String args[])
+{
+	System.out.println("The two number is:");
+	int a=10;
+	int b=5;
+	System.out.println(a+ " " +b);
+	int mul=a*b;
+	int div=a/b;
+	System.out.println("The mul is:"+mul);
+	System.out.println("The div is:"+div);
+}
+}
---
+++ Substraction.java
@@ -0,0 +1,11 @@
+class Substraction{
+public static void main(String args[])
+{
+	System.out.println("The two number is:");
+	int a=10;
+	int b=5;
+	System.out.println(a+ " " +b);
+	int sub=a-b;
+	System.out.println("The sub is:"+sub);
+}
+}
