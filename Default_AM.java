89cfd58d329f357b91f8107ceedc3f352bcf16e7
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-04-01 21:17:49 +0530

    Add files via upload

---
+++ Default_AM.java
@@ -0,0 +1,14 @@
+package Access_Mod;
+import package_eg.*;
+public class Default_AM {
+		public static void main(String args[]) {
+		
+		{
+				Default_AM_Tst obj=new Default_AM_Tst();
+				obj.show();
+			}
+
+	}
+}
+
+
