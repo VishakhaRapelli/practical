460ac9efbb9b8b41181794b8b99faafa3a117b17
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-04-01 21:24:45 +0530

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Object {
+	int S1_no=1;
+	String name="vishakha";
+	public static void main(String args[]) {
+		Object_classEg Obj= new Object_classEg();
+		System.out.println(Obj.S1_no);
+		System.out.println(Obj.name);
+		
+
+		
+	}
+
+
+}
